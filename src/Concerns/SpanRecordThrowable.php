2956f1b57c079cc9b52777898a9858ce21981ec6
Deeka Wong <8337659+huangdijia@users.noreply.github.com>
2025-02-07 09:00:28 +0800

    fix: update exception attribute keys in SpanRecordThrowable trait

--- src/Concerns/SpanRecordThrowable.php
+++ src/Concerns/SpanRecordThrowable.php
@@ -32,8 +32,8 @@ trait SpanRecordThrowable
             TraceAttributes::EXCEPTION_TYPE => get_class($e),
             TraceAttributes::EXCEPTION_MESSAGE => $e->getMessage(),
             TraceAttributes::EXCEPTION_STACKTRACE => $e->getTraceAsString(),
-            TraceAttributes::CODE_FUNCTION => $e->getFile() . ':' . $e->getLine(),
-            TraceAttributes::CODE_LINENO => $e->getLine(),
+            TraceAttributes::CODE_FUNCTION_NAME => $e->getFile() . ':' . $e->getLine(),
+            TraceAttributes::CODE_LINE_NUMBER => $e->getLine(),
         ]);
         $span->setStatus(StatusCode::STATUS_ERROR, $e->getMessage());
         $span->recordException($e);
